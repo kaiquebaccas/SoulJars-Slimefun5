3fe2684483025deea19c554e4a7e0688d089cb2c
intisy (Finn Birich) <finn@birich.de>
2026-07-08 00:07:50 +0200

    build: move gradle conventions to Slimefun5/gradle repo; slim addon build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,4 @@ group = "io.github.thebusybiscuit.souljars"
 description = "SoulJars is a Slimefun addon that lets you capture the souls of slain mobs in jars."
 
 // Shared Slimefun-addon build conventions (Java 8, spigot-api baseline, core dep, publish, shadow, version).
-apply(from = "https://raw.githubusercontent.com/Slimefun5/workflows/stable/slimefun-addon.gradle")
+apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/stable/slimefun-addon.gradle")
